40d3d6023d7385332bd9f54ad8a3acc39cd42798
Ruediger Gad <r.c.g@gmx.de>
2017-03-05 12:23:18 +0100

    Begin to add experimental code.

--- pcap/pcap.dsl.core/src/pcap/dsl/core/trace/PcapDslTrace.java
+++ pcap/pcap.dsl.core/src/pcap/dsl/core/trace/PcapDslTrace.java
@@ -1,7 +1,15 @@
 package pcap.dsl.core.trace;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.tracecompass.internal.tmf.pcap.core.trace.PcapTrace;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.exceptions.TmfTraceException;
 
 public class PcapDslTrace extends PcapTrace {
 
+	@Override
+    public synchronized void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type) throws TmfTraceException {
+		System.out.println("PcapDslTrace.initTrace(...)");
+        super.initTrace(resource, path, type);
+	}
 }
