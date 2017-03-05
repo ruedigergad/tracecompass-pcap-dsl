48a6caa6234d08fe7a78dfd31e1024a56f9e5406
Ruediger Gad <r.c.g@gmx.de>
2017-03-05 12:37:26 +0100

    Prepare to customize aspects.

--- pcap/pcap.dsl.core/src/pcap/dsl/core/trace/PcapDslTrace.java
+++ pcap/pcap.dsl.core/src/pcap/dsl/core/trace/PcapDslTrace.java
@@ -1,15 +1,40 @@
 package pcap.dsl.core.trace;
 
+import java.util.Collection;
+
 import org.eclipse.core.resources.IResource;
+import org.eclipse.tracecompass.internal.tmf.pcap.core.event.aspect.PcapDestinationAspect;
+import org.eclipse.tracecompass.internal.tmf.pcap.core.event.aspect.PcapProtocolAspect;
+import org.eclipse.tracecompass.internal.tmf.pcap.core.event.aspect.PcapReferenceAspect;
+import org.eclipse.tracecompass.internal.tmf.pcap.core.event.aspect.PcapSourceAspect;
 import org.eclipse.tracecompass.internal.tmf.pcap.core.trace.PcapTrace;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.event.aspect.ITmfEventAspect;
+import org.eclipse.tracecompass.tmf.core.event.aspect.TmfBaseAspects;
 import org.eclipse.tracecompass.tmf.core.exceptions.TmfTraceException;
+
+import com.google.common.collect.ImmutableList;
 
 public class PcapDslTrace extends PcapTrace {
 
+    private static final Collection<ITmfEventAspect<?>> PCAP_DSL_ASPECTS =
+            ImmutableList.of(
+                    TmfBaseAspects.getTimestampAspect(),
+                    PcapSourceAspect.INSTANCE,
+                    PcapDestinationAspect.INSTANCE,
+                    PcapReferenceAspect.INSTANCE,
+                    PcapProtocolAspect.INSTANCE,
+                    TmfBaseAspects.getContentsAspect()
+                    );
+    
 	@Override
     public synchronized void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type) throws TmfTraceException {
 		System.out.println("PcapDslTrace.initTrace(...)");
         super.initTrace(resource, path, type);
 	}
+	
+    @Override
+    public Iterable<ITmfEventAspect<?>> getEventAspects() {
+        return PCAP_DSL_ASPECTS;
+    }
 }
