3175fc270eb588e3f22e631815f9426fb6add00f
Ruediger Gad <r.c.g@gmx.de>
2017-03-11 22:37:19 +0100

    Add Constants.java.

---
+++ pcap/pcap.dsl.core/src/pcap/dsl/core/config/Constants.java
@@ -0,0 +1,8 @@
+package pcap.dsl.core.config;
+
+public class Constants {
+
+	public static final String DSL_FILE_CONFIG_KEY = "pcap.dsl.core.dsl_expression";
+	public static final String DSL_FILE_CONFIG_LABEL = "DSL File";
+
+}
