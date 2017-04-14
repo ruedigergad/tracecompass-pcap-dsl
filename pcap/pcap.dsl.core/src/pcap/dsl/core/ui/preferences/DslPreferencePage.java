014f400074e449c11df841b895bb06ac2c223438
Ruediger Gad <r.c.g@gmx.de>
2017-04-14 18:30:58 +0200

    Fix preferences.

--- pcap/pcap.dsl.core/src/pcap/dsl/core/ui/preferences/DslPreferencePage.java
+++ pcap/pcap.dsl.core/src/pcap/dsl/core/ui/preferences/DslPreferencePage.java
@@ -7,39 +7,45 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
+import pcap.dsl.core.Activator;
 import pcap.dsl.core.config.Constants;
 
 public class DslPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
-	public DslPreferencePage() {
-		// TODO Auto-generated constructor stub
-	}
+    public DslPreferencePage() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public DslPreferencePage(int style) {
-		super(style);
-		// TODO Auto-generated constructor stub
-	}
+    public DslPreferencePage(int style) {
+        super(style);
+        // TODO Auto-generated constructor stub
+    }
 
-	public DslPreferencePage(String title, int style) {
-		super(title, style);
-		// TODO Auto-generated constructor stub
-	}
+    public DslPreferencePage(String title, int style) {
+        super(title, style);
+        // TODO Auto-generated constructor stub
+    }
 
-	public DslPreferencePage(String title, ImageDescriptor image, int style) {
-		super(title, image, style);
-		// TODO Auto-generated constructor stub
-	}
+    public DslPreferencePage(String title, ImageDescriptor image, int style) {
+        super(title, image, style);
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
+    @Override
+    public void init(IWorkbench workbench) {
+        setPreferenceStore(Activator.getDefault().getPreferenceStore());
+    }
 
-	}
-
-	@Override
-	protected void createFieldEditors() {
-		StringFieldEditor dslExpressionFileEditor = new FileFieldEditor(Constants.DSL_FILE_CONFIG_KEY, Constants.DSL_FILE_CONFIG_LABEL, getFieldEditorParent());
+    @Override
+    protected void createFieldEditors() {
+        StringFieldEditor dslExpressionFileEditor = new FileFieldEditor(Constants.DSL_FILE_CONFIG_KEY,
+                Constants.DSL_FILE_CONFIG_LABEL, getFieldEditorParent());
         addField(dslExpressionFileEditor);
-	}
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
 
 }
