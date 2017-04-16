a288dc863970a3bce486c97123df08cb301d5e63
Ruediger Gad <r.c.g@gmx.de>
2017-04-16 23:32:05 +0200

    First implementation for folding in DSL editor.

--- pcap/pcap.dsl.core/src/pcap/dsl/ui/view/DslEditorView.java
+++ pcap/pcap.dsl.core/src/pcap/dsl/ui/view/DslEditorView.java
@@ -1,9 +1,18 @@
 package pcap.dsl.ui.view;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentListener;
 import org.eclipse.jface.text.ITextListener;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.TextEvent;
@@ -44,6 +53,9 @@ public class DslEditorView extends TmfView {
 
     private ProjectionViewer dslEditorViewer;
     private IDocument dslEditorDocument;
+    private Pattern dslEditorFoldStartPattern = Pattern.compile("(?s)\\[\\s*\\[");
+    private Pattern dslEditorFoldEndPattern = Pattern.compile("(?s)\\]\\s*\\]");
+    private ProjectionAnnotation[] dslEditorOldAnnotations = new ProjectionAnnotation[] {};
 
     private ProjectionViewer outputViewer;
     private IDocument outputDocument;
@@ -65,11 +77,31 @@ public class DslEditorView extends TmfView {
 
             @Override
             public void textChanged(TextEvent event) {
+
+            }
+        });
+
+        IAnnotationAccess editorMarkerAnnotationAccess = new DefaultMarkerAnnotationAccess();
+        ProjectionSupport dslEditorProjectionSupport = new ProjectionSupport(this.dslEditorViewer,
+                editorMarkerAnnotationAccess, EditorsPlugin.getDefault().getSharedTextColors());
+        dslEditorProjectionSupport.install();
+
+        this.dslEditorViewer.doOperation(ProjectionViewer.TOGGLE);
+
+        this.dslEditorDocument = new Document();
+        ProjectionAnnotationModel pam = new ProjectionAnnotationModel();
+
+        this.dslEditorDocument.addDocumentListener(new IDocumentListener() {
+
+            @Override
+            public void documentChanged(DocumentEvent event) {
                 String dslExpression = dslEditorDocument.get();
 
                 if (dslExpression == null || dslExpression.isEmpty()) {
                     return;
                 }
+
+                updateDslEditorFolding();
 
                 try {
                     dslFn = DslHelper.generateProcessingFn(dslExpression);
@@ -83,27 +115,19 @@ public class DslEditorView extends TmfView {
                     e.printStackTrace();
                 }
             }
+
+            @Override
+            public void documentAboutToBeChanged(DocumentEvent event) {
+                // TODO Auto-generated method stub
+
+            }
         });
 
-        IAnnotationAccess editorMarkerAnnotationAccess = new DefaultMarkerAnnotationAccess();
-        ProjectionSupport dslEditorProjectionSupport = new ProjectionSupport(this.dslEditorViewer,
-                editorMarkerAnnotationAccess, EditorsPlugin.getDefault().getSharedTextColors());
-        dslEditorProjectionSupport.install();
-
-        this.dslEditorViewer.doOperation(ProjectionViewer.TOGGLE);
-
-        this.dslEditorDocument = new Document();
+        this.dslEditorViewer.enableProjection();
+        this.dslEditorViewer.setDocument(this.dslEditorDocument, pam);
+        this.dslEditorViewer.enableProjection();
+        this.dslEditorViewer.setDocument(this.dslEditorDocument, pam);
         this.dslEditorDocument.set(Helper.getDslExpression());
-        ProjectionAnnotationModel pam = new ProjectionAnnotationModel();
-
-        this.dslEditorViewer.enableProjection();
-        this.dslEditorViewer.setDocument(this.dslEditorDocument, pam);
-        this.dslEditorViewer.enableProjection();
-        this.dslEditorViewer.setDocument(this.dslEditorDocument, pam);
-
-        pam = this.dslEditorViewer.getProjectionAnnotationModel();
-        pam.addAnnotation(new ProjectionAnnotation(), new Position(0, 44));
-        pam.addAnnotation(new ProjectionAnnotation(), new Position(90, 150));
 
         CompositeRuler outputVerticalRuler = new CompositeRuler(RULER_WIDTH);
         outputVerticalRuler.addDecorator(0, new LineNumberRulerColumn());
@@ -125,11 +149,6 @@ public class DslEditorView extends TmfView {
         this.outputViewer.setDocument(this.outputDocument, pam2);
         this.outputViewer.enableProjection();
         this.outputViewer.setDocument(this.outputDocument, pam2);
-
-        pam2 = this.outputViewer.getProjectionAnnotationModel();
-        pam2.addAnnotation(new ProjectionAnnotation(), new Position(0, 44));
-        pam2.addAnnotation(new ProjectionAnnotation(), new Position(90, 150));
-
     }
 
     @Override
@@ -181,5 +200,98 @@ public class DslEditorView extends TmfView {
             String outString = DslHelper.prettyPrint(packetDataMap);
             this.outputDocument.set(outString.replaceAll(", ", "\n"));
         }
+    }
+
+    private void updateDslEditorFolding() {
+        ProjectionAnnotationModel annotationModel = this.dslEditorViewer.getProjectionAnnotationModel();
+        if (annotationModel == null) {
+            return;
+        }
+
+        Matcher foldStartMatcher = this.dslEditorFoldStartPattern.matcher(this.dslEditorDocument.get());
+        List<Position> foldStartPositions = new ArrayList<>();
+        while (foldStartMatcher.find()) {
+            foldStartPositions.add(new Position(foldStartMatcher.start()));
+        }
+
+        Matcher foldEndMatcher = this.dslEditorFoldEndPattern.matcher(this.dslEditorDocument.get());
+        List<Integer> foldEndIndices = new ArrayList<>();
+        while (foldEndMatcher.find()) {
+            foldEndIndices.add(foldEndMatcher.end());
+        }
+
+        System.out.println("Fold Start: " + String.valueOf(foldStartPositions));
+        System.out.println("Fold End: " + String.valueOf(foldEndIndices));
+
+        // foldStartPositions.get(0)
+        // .setLength(foldEndIndices.get(foldEndIndices.size() - 1) -
+        // foldStartPositions.get(0).getOffset());
+
+        Set<Integer> matchedStartIdx = new HashSet<>();
+        matchedStartIdx.add(0);
+        Set<Integer> matchedEndIdx = new HashSet<>();
+        matchedEndIdx.add(foldEndIndices.size() - 1);
+
+        int startIdx = 1;
+        int maxStartIdx = startIdx;
+        int endIdx = 0;
+        int currentStartOffset = foldStartPositions.get(startIdx).getOffset();
+        int currentEndOffset = foldEndIndices.get(endIdx);
+        while (startIdx >= 1 && endIdx >= 0 && startIdx < foldStartPositions.size() + 1
+                && endIdx < foldEndIndices.size() - 1) {
+            System.out.println(currentStartOffset + " < " + currentEndOffset);
+
+            if (currentStartOffset < currentEndOffset && maxStartIdx < foldStartPositions.size()) {
+                do {
+                    startIdx++;
+                } while (matchedStartIdx.contains(startIdx));
+
+                if (startIdx < foldStartPositions.size()) {
+                    currentStartOffset = foldStartPositions.get(startIdx).getOffset();
+                } else {
+                    currentStartOffset = Integer.MAX_VALUE;
+                }
+
+                continue;
+            } else if (maxStartIdx >= foldStartPositions.size()) {
+                while (matchedStartIdx.contains(startIdx)) {
+                    startIdx--;
+                }
+            }
+
+            matchedStartIdx.add(startIdx);
+
+            Position prevStartPosition = foldStartPositions.get(startIdx - 1);
+
+            maxStartIdx = Math.max(maxStartIdx, startIdx);
+
+            System.out.println(
+                    prevStartPosition.getOffset() + " - " + (currentEndOffset - prevStartPosition.getOffset()));
+            prevStartPosition.setLength(currentEndOffset - prevStartPosition.getOffset());
+
+            endIdx++;
+            if (foldEndIndices.size() < endIdx) {
+                endIdx = -1;
+                continue;
+            }
+            currentEndOffset = foldEndIndices.get(endIdx);
+
+            while (matchedStartIdx.contains(startIdx - 1)) {
+                startIdx--;
+                currentStartOffset = foldStartPositions.get(startIdx).getOffset();
+            }
+        }
+
+        System.out.println("FOOOOOOOOOOOOOOOOO" + String.valueOf(foldStartPositions));
+
+        ProjectionAnnotation[] currentAnnotations = new ProjectionAnnotation[foldStartPositions.size()];
+        Map<ProjectionAnnotation, Position> newAnnotations = new HashMap<>();
+        for (int i = 0; i < foldStartPositions.size(); i++) {
+            Position pos = foldStartPositions.get(i);
+            currentAnnotations[i] = new ProjectionAnnotation();
+            newAnnotations.put(currentAnnotations[i], pos);
+        }
+        annotationModel.modifyAnnotations(this.dslEditorOldAnnotations, newAnnotations, null);
+        this.dslEditorOldAnnotations = currentAnnotations;
     }
 }
